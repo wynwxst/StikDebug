024d54635f050fdb0cf6d0991975004cddf8d0c2
neoarz <tyrantneo740@gmail.com>
2025-03-31 14:29:13 -0400

    fix name and shortcut

--- StikJIT/Views/SettingsView.swift
+++ StikJIT/Views/SettingsView.swift
@@ -277,7 +277,7 @@ struct SettingsView: View {
                             
                             // Main Developers 
                             VStack(alignment: .leading, spacing: 12) {
-                                Text("Developers")
+                                Text("Creators")
                                     .font(.subheadline)
                                     .foregroundColor(.secondary)
                                 
@@ -335,7 +335,7 @@ struct SettingsView: View {
                             
                             // Collaborators in vertical stack
                             VStack(alignment: .leading, spacing: 12) {
-                                Text("Collaborators")
+                                Text("Developers")
                                     .font(.subheadline)
                                     .foregroundColor(.secondary)
                                 
@@ -373,6 +373,9 @@ struct SettingsView: View {
                                 
                                 // New Discord Link
                                 LinkRow(icon: "bubble.left", title: "Join Discord", url: "https://discord.gg/ZnNcrRT3M8")
+                                
+                                // Your new shortcut link
+                                LinkRow(icon: "sparkles", title: "iOS Shortcut", url: "https://www.icloud.com/shortcuts/f0c11c0a76654e63b18d8c59d82e152e")
                                 
                                 // StikNES promotion
                                 Button(action: {
