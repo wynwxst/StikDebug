702a10795b84a8a29156ee28dc41db9cb8a35b0c
neoarz <tyrantneo740@gmail.com>
2025-04-14 22:49:17 -0400

    start

---
+++ update_json.py
@@ -0,0 +1,18 @@
+import json
+import re
+import requests
+import os
+import sys
+from datetime import datetime
+
+def prepare_description(text):
+    text = re.sub('<[^<]+?>', '', text) # Remove HTML tags
+    text = re.sub(r'#{1,6}\s?', '', text) # Remove markdown header tags
+    text = re.sub(r'\*{2}', '', text) # Remove all occurrences of two consecutive asterisks
+    text = re.sub(r'(?<=\r|\n)-', '•', text) # Only replace - with • if it is preceded by \r or \n
+    text = re.sub(r'`', '"', text) # Replace ` with "
+    text = re.sub(r'\r\n\r\n', '\r \n', text) # Replace \r\n\r\n with \r \n (avoid incorrect display of the description regarding paragraphs)
+    return text
+
+
+
